36bad3775bcb854f5b622f9c178298ed97a46bdf
VelizarKD <79282909+VelizarKD@users.noreply.github.com>
2026-05-04 14:08:20 +0300

    arrays exercise

---
+++ src/main/java/org/example/Arrays/E02_PrintNumInReverseOrder.java
@@ -0,0 +1,22 @@
+package org.example.Arrays;
+
+import java.util.Scanner;
+
+public class E02_PrintNumInReverseOrder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        int[] numbers = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(scanner.nextLine());
+            numbers[i] = num;
+        }
+
+        for (int i = numbers.length - 1; i >= 0; i--) {
+            System.out.print(numbers[i] + " ");
+
+        }
+    }
+}
