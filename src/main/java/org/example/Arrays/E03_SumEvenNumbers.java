2bb72f203f5dae84563244f9220b0d81277aa140
VelizarKD <79282909+VelizarKD@users.noreply.github.com>
2026-05-06 13:27:03 +0300

    arrays exercise

---
+++ src/main/java/org/example/Arrays/E03_SumEvenNumbers.java
@@ -0,0 +1,23 @@
+package org.example.Arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class E03_SumEvenNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] numbers = Arrays.stream(scanner.nextLine().split(" "))
+                .mapToInt(e -> Integer.parseInt(e))
+                .toArray();
+
+        int sum = 0;
+
+        for (int i = 0; i <= numbers.length - 1; i++) {
+            if (numbers[i] % 2 == 0) {
+                sum += numbers[i];
+            }
+        }
+        System.out.println(sum);
+    }
+}
