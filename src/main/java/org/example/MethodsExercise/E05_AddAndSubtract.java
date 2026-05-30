40cc47172ee7327aeb1e9e12d1c5f70f00d5eb1a
VelizarKD <79282909+VelizarKD@users.noreply.github.com>
2026-05-30 16:50:31 +0300

    methods exercise

--- src/main/java/org/example/MethodsExercise/E05_AddAndSubtract.java
+++ src/main/java/org/example/MethodsExercise/E05_AddAndSubtract.java
@@ -1,4 +1,26 @@
 package org.example.MethodsExercise;
 
+import java.util.Scanner;
+
 public class E05_AddAndSubtract {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number1 = Integer.parseInt(scanner.nextLine());
+        int number2 = Integer.parseInt(scanner.nextLine());
+        int number3 = Integer.parseInt(scanner.nextLine());
+
+        int firstExample = getSum(number1, number2);
+        int secondExample = getSubtract(firstExample, number3);
+
+        System.out.println(secondExample);
+    }
+
+
+    private static int getSum(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+    private static int getSubtract(int number, int number1) {
+        return number - number1;
+    }
 }
