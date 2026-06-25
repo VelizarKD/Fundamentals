47822c10dd0a7bdaf9535c3b5a9005aab0961334
VelizarKD <79282909+VelizarKD@users.noreply.github.com>
2026-06-25 17:11:45 +0300

    lists exercise

--- src/main/java/org/example/Lists/E05_ListManipulationAdvanced.java
+++ src/main/java/org/example/Lists/E05_ListManipulationAdvanced.java
@@ -1,9 +1,49 @@
 package org.example.Lists;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class E05_ListManipulationAdvanced {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbersList = Arrays.stream(scanner.nextLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        String input = scanner.nextLine();
+
+        while (!input.equals("end")) {
+            List<String> commandLine = Arrays.stream(input.split(" ")).collect(Collectors.toList());
+            String command = commandLine.get(0);
+
+            switch (command) {
+                case "Contains":
+                    int containsNumber = Integer.parseInt(commandLine.get(1));
+                    if (numbersList.contains(containsNumber)) {
+                        System.out.println("Yes");
+                    } else {
+                        System.out.println("No such number");
+                    }
+                    break;
+                case "Print even":
+
+                    break;
+                case "Print odd":
+
+                    break;
+                case "Get sum":
+
+                    break;
+                case "Filter":
+
+                    break;
+            }
+
+            input = scanner.nextLine();
+        }
+        System.out.println();
     }
 }
