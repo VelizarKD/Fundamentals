429d4f631c652399b59f95dab97ec446c33065c1
VelizarKD <79282909+VelizarKD@users.noreply.github.com>
2026-06-27 00:12:48 +0300

    lists exercise

--- src/main/java/org/example/Lists/E06_RemoveNegativesAndReverse.java
+++ src/main/java/org/example/Lists/E06_RemoveNegativesAndReverse.java
@@ -1,9 +1,28 @@
 package org.example.Lists;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class E06_RemoveNegativesAndReverse {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numList = Arrays
+                .stream(scanner.nextLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        numList.removeIf(e -> e < 0);
+        Collections.reverse(numList);
+
+
+        if (numList.isEmpty()) {
+            System.out.println("empty");
+        } else {
+            System.out.println(numList.toString().replaceAll("[\\[\\],]", ""));
+        }
     }
 }
