d9946fdd85351ea2cc6376a7617dc2768f5cf8fe
VelizarKD <79282909+VelizarKD@users.noreply.github.com>
2026-06-28 12:11:16 +0300

    lists exercise

---
+++ src/main/java/org/example/ListsExercise/E03_HouseParty.java
@@ -0,0 +1,42 @@
+package org.example.ListsExercise;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class E03_HouseParty {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int countCommands = Integer.parseInt(scanner.nextLine());
+        List<String> guestList = new ArrayList<>();
+
+        for (int count = 0; count < countCommands; count++) {
+            String command = scanner.nextLine();
+
+            List<String> commandParts = Arrays.stream(command.split(" ")).collect(Collectors.toList());
+            String name = commandParts.get(0);
+
+            if (commandParts.size() == 3) {
+                if (guestList.contains(name)) {
+                    System.out.printf("%s is already in the list!%n", name);
+                } else {
+                    guestList.add(name);
+                }
+
+            } else if (commandParts.size() == 4) {
+                if (guestList.contains(name)) {
+                    guestList.remove(name);
+                } else {
+                    System.out.printf("%s is not in the list!%n", name);
+                }
+            }
+
+        }
+        for (String name : guestList) {
+            System.out.println(name);
+        }
+    }
+}
